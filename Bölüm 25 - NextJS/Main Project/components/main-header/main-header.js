2362feb2a24d593dc9b91dbabfa261d1d5b6b97a
bertan <bertanerdogan@windowslive.com>
2024-09-22 15:21:51 +0300

    NextJS Practice

---
+++ Bölüm 25 - NextJS/Main Project/components/main-header/main-header.js
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
+
+export default function MainHeader() {
+  const path = usePathname();
+
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/public" className={classes.logo}>
+          <Image src={logoImg} alt="NextLevel Food" />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
